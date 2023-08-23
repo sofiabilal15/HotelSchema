3fb0afa0fa4aac252a4fe9f7847cc46d1f86419c
Sofia Bilal <74833474+sofiabilal15@users.noreply.github.com>
2023-08-23 12:23:04 +0100

    Add files via upload

---
+++ HotelSchema/Sofia-HotelDB.sql
@@ -0,0 +1,52 @@
+DROP DATABASE IF EXISTS SofiaHotelDB;
+
+CREATE DATABASE SofiaHotelDB;
+
+USE SofiaHotelDB;
+
+CREATE TABLE Room (
+    RoomNumber INT PRIMARY KEY,
+    Type VARCHAR(50),
+    ADAAccessible VARCHAR(3),
+    StandardOccupancy INT,
+    MaximumOccupancy INT,
+    BasePrice DECIMAL(10, 2),
+    ExtraPerson DECIMAL(5, 2)
+);
+
+CREATE TABLE Guest (
+    GuestID INT PRIMARY KEY,
+    Name VARCHAR(100),
+    Address VARCHAR(255),
+    City VARCHAR(50),
+    State VARCHAR(50),
+    Zip VARCHAR(20),
+    Phone VARCHAR(20)
+);
+
+CREATE TABLE Reservation (
+    ReservationID INT PRIMARY KEY,
+    RoomNumber INT,
+    GuestID INT,
+    Adults INT,
+    Children INT,
+    StartDate DATE,
+    EndDate DATE,
+    TotalRoomCost DECIMAL(10, 2),
+    FOREIGN KEY (RoomNumber) REFERENCES Room(RoomNumber),
+    FOREIGN KEY (GuestID) REFERENCES Guest(GuestID)
+);
+
+CREATE TABLE Amenities (
+    RoomAmenityID INT PRIMARY KEY,
+    Amenity VARCHAR(255)
+);
+
+
+CREATE TABLE Room_Amenities (
+    RoomNumber INT,
+    RoomAmenityID INT,
+    PRIMARY KEY (RoomNumber, RoomAmenityID),
+    FOREIGN KEY (RoomNumber) REFERENCES Room(RoomNumber),
+    FOREIGN KEY (RoomAmenityID) REFERENCES Amenities(RoomAmenityID)
+);
